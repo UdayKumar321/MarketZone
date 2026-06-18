0a6381af4d17d5db66937f722d23334e470d9cd2
UdayKumar321 <your-github-email@gmail.com>
2026-06-18 19:32:52 +0530

    Restore ProductDetail route

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import Orders from "./pages/Orders";
 import SellerDashboard from "./pages/SellerDashboard";
@@ -16,13 +17,21 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
+
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
+
           <Route path="products" element={<Products />} />
+          <Route path="products/:id" element={<ProductDetail />} />
+
           <Route path="cart" element={<Cart />} />
           <Route path="wishlist" element={<Wishlist />} />
           <Route path="orders" element={<Orders />} />
-          <Route path="seller/dashboard" element={<SellerDashboard />} />
+
+          <Route
+            path="seller/dashboard"
+            element={<SellerDashboard />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
